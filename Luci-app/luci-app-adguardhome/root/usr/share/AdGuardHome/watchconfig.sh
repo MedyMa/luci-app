46e1f6f846b133f773e99afdd91ed9e59c43f347
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-06-12 23:22:05 +0800

    Add files via upload

--- Luci-app/luci-app-adguardhome/root/usr/share/AdGuardHome/watchconfig.sh
+++ Luci-app/luci-app-adguardhome/root/usr/share/AdGuardHome/watchconfig.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-configpath=$(uci get AdGuardHome.AdGuardHome.configpath)
+configpath=$(uci -q get AdGuardHome.AdGuardHome.configpath 2>/dev/null)
+redirect=$(uci -q get AdGuardHome.AdGuardHome.redirect 2>/dev/null)
 while :
 do
 	sleep 10
-	if [ -f "$configpath" ]; then
+	configpath=$(uci -q get AdGuardHome.AdGuardHome.configpath 2>/dev/null)
+	redirect=$(uci -q get AdGuardHome.AdGuardHome.redirect 2>/dev/null)
+	if [ "$redirect" = "none" ]; then
+		break
+	fi
+	if [ -r "$configpath" ] && /etc/init.d/AdGuardHome isrunning >/dev/null 2>&1; then
 		/etc/init.d/AdGuardHome do_redirect 1
 		break
 	fi
